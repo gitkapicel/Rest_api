8c7eefeccba7f7f96fb3a909cebb92a6d6b0dae6
Łukasz Tracz <kapicel20@wp.pl>
2015-11-23 00:19:17 +0100

    Po uruchomieniu  http://localhost:2402/api/workerr ddla wszystkich , http://localhost:2402/api/workerr/1 dla id 1

---
+++ WebApi/Controllers/WorkerrController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class WorkerrController : ApiController
+    {
+
+        Workers[] workers = new Workers[] 
+        { 
+            new Workers { idworker = 1, name = "Tom", surname = "Smith"}, 
+            new Workers { idworker = 2, name = "Jack", surname = "Lee"}, 
+            new Workers { idworker = 3, name = "Alex", surname = "Jackson"} 
+        };
+
+
+
+        public IEnumerable<Workers> GetAllWorkers()
+        {
+            return workers;
+        }
+
+        public IHttpActionResult GetWorker(int id)
+        {
+            var worker = workers.FirstOrDefault((p) => p.idworker == id);
+            if (worker == null)
+            {
+                return NotFound();
+            }
+            return Ok(worker);
+        }
+
+    }
+
+     
+
+
+
+}
---
+++ WebApi/Models/Workers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class Workers
+    {
+        public int idworker { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+
+    }
+}
